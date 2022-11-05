cb5175d98100af8e00d322abab656807612fb9d8
i9f <smadenoshijian@gmail.com>
2022-11-05 23:40:40 +0600

    feat: add mobile navigation

---
+++ mobile-nav.js
@@ -0,0 +1,13 @@
+const mobileNavButton = document.querySelector(".header__mobile-btn");
+const mobileNavIcon = document.querySelector(".header__mobile-icon");
+const mobileNav = document.querySelector(".header__mobile-nav");
+
+function mobile_menu() {
+  mobileNavButton.addEventListener("click", () => {
+    mobileNavIcon.classList.toggle("active");
+    mobileNav.classList.toggle("active");
+    document.body.classList.toggle("hidden");
+  });
+}
+
+mobile_menu();
